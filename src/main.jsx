719b75c00361eb39f013e238145828b1c51dd197
Lautaro <lautic003@gmail.com>
2026-02-05 15:45:48 -0300

    fix: github pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import App from "./App.jsx";
 import { CartProvider } from "./store/CartProvider.jsx";
 import "./styles/global.module.css";
 
+const baseUrl = import.meta.env.BASE_URL;
+const basename = baseUrl === "/" ? "/" : baseUrl.replace(/\/$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <CartProvider>
         <App />
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
-
