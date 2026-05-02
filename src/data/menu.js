2f2a8717eb276a602f4b3314ec589023511f150b
Lautaro <lautic003@gmail.com>
2026-05-01 21:36:58 -0300

    fix: deshabilitar lautiboom y bbqueen

--- src/data/menu.js
+++ src/data/menu.js
@@ -40,7 +40,7 @@ export const burgers = [
       { id: "caramelized_onion", label: "Cebolla caramelizada" },
     ],
     img: "/burgers/lautiboom.svg",
-    isAvailable: 1,
+    isAvailable: 0,
   },
   {
     id: "american",
@@ -88,7 +88,7 @@ export const burgers = [
       { id: "caramelized_onion", label: "Cebolla caramelizada" },
     ],
     img: "/burgers/bbqueen.svg",
-    isAvailable: 1,
+    isAvailable: 0,
   },
   {
     id: "smoklahoma",
